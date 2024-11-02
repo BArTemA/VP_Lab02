0eb37a5cb76e74da81cebb48294c66f1e85ce6d2
Aleksander <evdokimov.aleksandr.2004@gmail.com>
2024-11-02 22:04:49 +0300

    Added namespace to Task2

--- Task2/GroupSt.cs
+++ Task2/GroupSt.cs
@@ -1,87 +1,87 @@
-﻿using System.Threading.Tasks;
-
-namespace Task2;
-using Task1;
-public class GroupSt
+﻿using Task1;
+namespace Task2
 {
-    public int groupNum { get; set; }
-    private List<Student> Students { get; set; }
-    public GroupSt(int gn)
+    public class GroupSt
     {
-        groupNum = gn;
-        Students = new List<Student>();
-    }
-
-    public static void Add(GroupSt group, Student student)
-    {
-        if (student != null)
+        public int groupNum { get; set; }
+        private List<Student> Students { get; set; }
+        public GroupSt(int gn)
         {
-            bool exists = false;
+            groupNum = gn;
+            Students = new List<Student>();
+        }
+
+        public static void Add(GroupSt group, Student student)
+        {
+            if (student != null)
+            {
+                bool exists = false;
+                foreach (var s in group.Students)
+                {
+                    if (s.id == student.id)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (!exists)
+                {
+                    group.Students.Add(student);
+                }
+            }
+        }
+
+        public static void Remove(GroupSt group, int studentID)
+        {
+            Student studentToRemove = null;
             foreach (var s in group.Students)
             {
-                if (s.id == student.id)
+                if (s.id == studentID)
                 {
-                    exists = true;
+                    studentToRemove = s;
                     break;
                 }
             }
 
-            if (!exists)
+            if (studentToRemove != null)
             {
-                group.Students.Add(student);
-            }
-        }
-    }
-
-    public static void Remove(GroupSt group, int studentID)
-    {
-        Student studentToRemove = null;
-        foreach (var s in group.Students)
-        {
-            if (s.id == studentID)
-            {
-                studentToRemove = s;
-                break;
+                group.Students.Remove(studentToRemove);
             }
         }
 
-        if (studentToRemove != null)
+        public static Student GetStudent(GroupSt group, int studentID)
         {
-            group.Students.Remove(studentToRemove);
-        }
-    }
-
-    public static Student GetStudent(GroupSt group, int studentID)
-    {
-        foreach (var s in group.Students)
-        {
-            if (s.id == studentID)
+            foreach (var s in group.Students)
             {
-                return s;
+                if (s.id == studentID)
+                {
+                    return s;
+                }
             }
+            return null;
         }
-        return null;
-    }
 
-    public static string GetInfo(GroupSt group)
-    {
-        List<Student> sortedStudents = new List<Student>(group.Students);
-        sortedStudents.Sort((s1, s2) =>
+        public static string GetInfo(GroupSt group)
         {
-            int lastNameComparison = s1.lastName.CompareTo(s2.lastName);
-            if (lastNameComparison == 0)
+            List<Student> sortedStudents = new List<Student>(group.Students);
+            sortedStudents.Sort((s1, s2) =>
             {
-                return s1.firstName.CompareTo(s2.firstName);
+                int lastNameComparison = s1.lastName.CompareTo(s2.lastName);
+                if (lastNameComparison == 0)
+                {
+                    return s1.firstName.CompareTo(s2.firstName);
+                }
+                return lastNameComparison;
+            });
+
+            List<string> studentInfo = new List<string>();
+            foreach (var s in sortedStudents)
+            {
+                studentInfo.Add(s.ToString());
             }
-            return lastNameComparison;
-        });
 
-        List<string> studentInfo = new List<string>();
-        foreach (var s in sortedStudents)
-        {
-            studentInfo.Add(s.ToString());
+            return $"Номер группы: {group.groupNum}\n Студенты:\n{string.Join("\n", studentInfo)}";
         }
-
-        return $"Номер группы: {group.groupNum}\n Студенты:\n{string.Join("\n", studentInfo)}";
     }
 }
